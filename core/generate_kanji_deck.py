a6a9c81ab985418c424a11f576c4e30fe5a7f9ae
Adam Johnson <riizade@gmail.com>
2022-12-16 22:56:54 +0900

    have kanji dict iteration working

--- core/generate_kanji_deck.py
+++ core/generate_kanji_deck.py
@@ -3,10 +3,10 @@ import sys
 
 def generate():
     jam = Jamdict()
-    for kanji in jam.kd2.all_ne_type():
-        sys.stdout.buffer.write(kanji)
-        sys.stdout.buffer.write("\n".encode("utf8"))
-
-    for kanji in jam.kd2.all_pos():
-        sys.stdout.buffer.write(kanji)
-        sys.stdout.buffer.write("\n".encode("utf8"))
+    sqlite_context = jam.kd2.ctx()
+    rows = sqlite_context.select("SELECT * FROM character")
+    for row in rows:
+        if row['freq'] is not None:
+            for field in ['ID', 'literal', 'stroke_count', 'grade', 'freq', 'jlpt']:
+                    sys.stdout.buffer.write((str(row[field]) + " ").encode("utf8"))
+            sys.stdout.buffer.write("\n".encode("utf8"))
