b39f73b605f6d719592c8612ae959a339c1b13d9
Adam Johnson <riizade@gmail.com>
2022-12-16 15:37:51 +0900

    reading .apkg files is broken (unnecessary sql constraint)

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from ankisync2 import Apkg
+import sys
 
 
 def main():
-    pass
+    apkg = Apkg("scratch/N0.apkg")
+    print("bleh")
+    for note in apkg.db.Notes:
+        sys.stdout.buffer.write(str(note.data).encode("utf-8"))
+
+    apkg.export("scratch/N0-updated.apkg")
 
 if __name__ == "__main__":
     main()
