543e9e73a1af77f51c453f9e0b0d8a19a4b846d0
Adam Johnson <riizade@gmail.com>
2022-12-18 13:17:32 +0900

    test cache load

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from core.kanji import load_all_kanji
 from core.vocab import load_all_vocab
+from core.utils import cached_load
+from pathlib import Path
 
 def main():
-    load_all_kanji()
+    cached_load(load_all_kanji, Path('./cache/kanji.bin'))
 
 if __name__ == "__main__":
     main()
